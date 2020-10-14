738fde155eda911fba267f8babb674b77355827d
Jose Estrada <joseestradacobo@gmail.com>
2020-10-14 09:27:23 +0200

    Primeros Ejercicios

---
+++ Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    System.out.println("Jose Estrada");
+    System.out.println("Juan de Robles");
+    System.out.println("555 55 55 55");
+  }
+}
---
+++ Ejercicio4.java
@@ -0,0 +1,11 @@
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    System.out.println("Lunes\tMartes\tMiérc\tJueves\tViernes");
+    System.out.println("EDD\tPROG\tPROG\tPROG\tFOL");
+    System.out.println("LM\tPROG\tPROG\tLM\tFOL");
+    System.out.println("LM\tEED\tPROG\tLM\tFOL");
+    System.out.println("PRO\tEDD\tBBDD\tSINF\tSINF");
+    System.out.println("PRO\tBBDD\tBBDD\tSINF\tSINF");
+    System.out.println("PRO\tBBDD\tBBDD\tSINF\tSINF");
+  }
+}
