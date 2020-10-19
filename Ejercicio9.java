065e5a05bf81be066be253c4c5ffbbd1cda805be
Jose Estrada <joseestradacobo@gmail.com>
2020-10-19 12:33:41 +0200

    Ejercicio Faltantes tema 1

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -2,12 +2,12 @@ public class Ejercicio9 {
 
   public static void main(String[] args) {
     
-    System.out.println("       _                     ______       _                    _        ");
-    System.out.println("      | |                   |  ____|     | |                  | |       ");    
-    System.out.println("      | |  ___   ___   ___  | |__    ___ | |_  _ __  __ _   __| |  __ _ ");
-    System.out.println("  _   | | / _ \\ / __| / _ \\ |  __|  / __|| __|| '__|/ _` | / _` | / _` |");
-    System.out.println(" | |__| || (_) |\\__ \\|  __/ | |____ \\__ \\| |_ | |  | (_| || (_| || (_| |");
-    System.out.println("  \\____/  \\___/ |___/ \\___| |______||___/ \\__||_|   \\__,_| \\__,_| \\__,_|");
+    System.out.println("       _                      ______       _                    _        ");
+    System.out.println("      | |                    |  ____|     | |                  | |       ");    
+    System.out.println("      | |  ___   ___   ___   | |__    ___ | |_  _ __  __ _   __| |  __ _ ");
+    System.out.println("  _   | | / _ \\ / __| / _ \\  |  __|  / __|| __|| '__|/ _` | / _` | / _` |");
+    System.out.println(" | |__| || (_) |\\__ \\|  __/  | |____ \\__ \\| |_ | |  | (_| || (_| || (_| |");
+    System.out.println("  \\____/  \\___/ |___/ \\___|  |______||___/ \\__||_|   \\__,_| \\__,_| \\__,_|");
   }
 
 }   
