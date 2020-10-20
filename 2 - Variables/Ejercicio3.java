7ec608dae55d22ee654afac40d6346de3b57c1c5
Jose Estrada <joseestradacobo@gmail.com>
2020-10-20 10:19:58 +0200

    Organizacion + Nuevos ejercicios

---
+++ 2 - Variables/Ejercicio3.java
@@ -0,0 +1,11 @@
+public class Ejercicio3 {
+  public static void main(String[] args) {
+    String nombre = "Jose Estrada";
+    String direccion = "Juan de Robles";
+    String telefono = "655555555";
+
+    System.out.println(nombre);
+    System.out.println(direccion);
+    System.out.println(telefono);
+  }
+}
