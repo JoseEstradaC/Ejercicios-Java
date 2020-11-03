1089df500254fdee01cff35a890bb5e763901596
Jose Estrada <joseestradacobo@gmail.com>
2020-11-03 10:03:03 +0100

    Update Ejercicio01.java

--- 5 - Bucles/Ejercicio01.java
+++ 5 - Bucles/Ejercicio01.java
@@ -1,7 +1,7 @@
 public class Ejercicio01 {
   public static void main(String[] args) {
 
-    for (int i = 0; i <= 100; i++) {
+    for (int i = 0; i <= 20; i++) {
       System.out.println(5 * i);
     }
 
