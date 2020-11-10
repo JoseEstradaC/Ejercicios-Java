e6f7350b5dde37b2aaa6211bf3b0efd478155eb3
Jose Estrada <joseestradacobo@gmail.com>
2020-11-10 08:48:44 +0100

    Create Ejercicio08.java

---
+++ 5 - Bucles/Ejercicio08.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio08 {
+  public static void main(String[] args) {
+
+    System.out.println("Tabla multiplicar del: ");
+
+    Scanner sc = new Scanner(System.in);
+    int tablaMultiplicar = Integer.parseInt(sc.nextLine());
+    sc.close();
+    
+    for (int i = 0; i <= 10; i++) {
+      System.out.printf("%2d * %2d = %3d\n", tablaMultiplicar, i, (tablaMultiplicar * i));
+    }
+
+  }
+}
