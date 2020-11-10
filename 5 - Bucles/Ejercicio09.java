aec60cc3d97bc7cb96d17a25e1823f5d76a6f714
Jose Estrada <joseestradacobo@gmail.com>
2020-11-10 08:56:27 +0100

    Create Ejercicio09.java

---
+++ 5 - Bucles/Ejercicio09.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio09 {
+  public static void main(String[] args) {
+
+    System.out.println("Introduzca un numero entero: ");
+
+    Scanner sc = new Scanner(System.in);
+    int numero = Integer.parseInt(sc.nextLine());
+    sc.close();
+
+    int cantidadDigitos = 0;
+    
+    while (!(numero == 0)) {
+      cantidadDigitos++;
+      numero = numero / 10;
+    }
+    System.out.println("La cantidad de dígitos del numero es de: " + cantidadDigitos);
+
+  }
+}
