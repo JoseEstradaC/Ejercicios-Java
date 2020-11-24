fa5189b0a2abf5bd109483f0d245b7ff9a85cbef
Jose Estrada <joseestradacobo@gmail.com>
2020-11-24 10:07:45 +0100

    Create Ejercicio03.java

---
+++ 6 - Números aleatorios/Ejercicio03.java
@@ -0,0 +1,51 @@
+
+public class Ejercicio03 {
+  public static void main(String[] args) {
+
+    int numeroPalo = (int) (Math.random() * 4) + 1;
+
+    String palo = "";
+
+    switch (numeroPalo) {
+      case 1:
+        palo = "Bastos";
+        break;
+      case 2:
+        palo = "Copas";
+        break;
+      case 3:
+        palo = "Oros";
+        break;
+      case 4:
+        palo = "Espadas";
+        break;
+
+      default:
+        break;
+    }
+
+    int numeroCarta = (int) (Math.random() * 10) + 1;
+    String carta = "";
+
+    switch (numeroCarta) {
+      case 1:
+        carta = "As";
+        break;
+      case 11:
+        carta = "Sota";
+        break;
+      case 12:
+        carta = "Caballo";
+        break;
+      case 13:
+        carta = "Rey";
+        break;
+
+      default:
+        carta = String.valueOf(numeroCarta);
+        break;
+    }
+
+    System.out.println("Carta aleatoria: " + carta + " de " + palo);
+  }
+}
