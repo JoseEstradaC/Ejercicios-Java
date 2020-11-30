b821ca1d8fe5692a24c7216556ca03f9a820e394
Jose Estrada <joseestradacobo@gmail.com>
2020-11-30 12:18:25 +0100

    Bucles

---
+++ 5 - Bucles/Ejercicio20.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ejercicio20 {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Introduzca un la altura de la pirámide: ");
+    int altura = Integer.parseInt(sc.nextLine());
+    System.out.print("Introduzca un carácter: ");
+    String caracter = sc.nextLine();
+    sc.close();
+
+    int espacios = altura - 1;
+    int caracteresAPirtar = 1;
+    for (int i = 0; i < altura; i++) {
+      for (int j = 0; j < espacios; j++) {
+        System.out.print(" ");
+      }
+      for (int h = 0; h < caracteresAPirtar; h++) {
+        if (h == 0 || h == caracteresAPirtar - 1) {
+          System.out.print(caracter);
+        } else {
+          if (i == altura - 1) {
+            System.out.print(caracter);
+          } else {
+            System.out.print(" ");
+          }
+
+        }
+      }
+      espacios--;
+      caracteresAPirtar = caracteresAPirtar + 2;
+
+      System.out.println();
+    }
+  }
+}
