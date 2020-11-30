8ab1b5a9d898129eef7914ac093c00ea33e4accf
Jose Estrada <joseestradacobo@gmail.com>
2020-11-30 12:49:52 +0100

    Nuevos ejercicios

---
+++ 6 - Números aleatorios/Ejercicio04.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio04 {
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 10; i++) {
+      int numeroAleatorio = (int) (Math.random() * (10 + 1));
+      System.out.print(numeroAleatorio + "  ");
+    }
+
+  }
+}
