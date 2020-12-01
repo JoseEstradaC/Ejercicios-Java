4b986ac89511b7957155a8837d77e825ade1d7e9
Jose Estrada <joseestradacobo@gmail.com>
2020-12-01 09:25:06 +0100

    Nuevos Ejercicios

---
+++ 6 - Números aleatorios/Ejercicio07.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio07 {
+  public static void main(String[] args) {
+
+    int numeroAleatorio = (int) (Math.random() * (3));
+    System.out.println(numeroAleatorio);
+
+  }
+}
---
+++ 6 - Números aleatorios/Ejercicio09.java
@@ -0,0 +1,19 @@
+
+public class Ejercicio09 {
+  public static void main(String[] args) {
+
+    boolean fin = false;
+
+    while (!fin) {
+      int numeroAleatorio = (int) (Math.random() * (101));
+      if (numeroAleatorio % 2 == 0) {
+
+        System.out.println(numeroAleatorio);
+        if (numeroAleatorio == 24) {
+          fin = true;
+        }
+      }
+    }
+
+  }
+}
